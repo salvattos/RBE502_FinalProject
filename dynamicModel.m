6b4920ac6f42a933d8b9f17ed9d2ae27d3910168
salvattos <swsalvatto@wpi.edu>
2022-12-13 17:39:55 -0500

    Finished ODE

--- dynamicModel.m
+++ dynamicModel.m
@@ -1,4 +1,5 @@
 %% RBE502 Final Project
+%% Trajectory Generation
 % Constants
 m = .027;L=.046;Ix=16.571710E-6;Iy=16.571710E-6;Iz=29.261652E-6; %L & m might need to be converted back to mm/g as given
 Ip=12.65625E-8;Kf=1.28192E-8;Km=5.964552E-3;Wmax=2618;Wmin=0;
@@ -47,10 +48,108 @@ for t = 0:.1:5
 end
 
 
-plot4(0:.1:5,xLog.',{'X Pos','X Vel','X Accel'})
-figure;
-plot3(xLog,yLog,zLog)
-grid on
+% plot4(0:.1:5,xLog.',{'X Pos','X Vel','X Accel'})
+% figure;
+% plot3(xLog,yLog,zLog)
+% grid on
+
+%% Simulation
+n = 12;
+
+A = zeros(2*n);
+B = zeros(2*n,n);
+A(1:n,n+1:end) = eye(n);
+B(n+1:end,:) = eye(n);
+lambdaVals = [-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4];
+
+K = place(A,B,lambdaVals);
+
+Acl = A-B*K;
+Q = eye(2*n).*1;
+P = lyap(Acl',Q);
+rho = 10;
+
+T = [0, 5];
+y0 = [P0.',0,0,0,0,0,0,0,0,0];
+
+[t,y] = ode45(@(t,x) odeFunc(t,x,K,P,rho,P0,P1,5),T,y0);
+
+function dX = odeFunc(t,X,K,P,rho,P0,PF,runTime)
+% Constants
+m = .027;L=.046;Ix=16.571710E-6;Iy=16.571710E-6;Iz=29.261652E-6; %L & m might need to be converted back to mm/g as given
+Ip=12.65625E-8;Kf=1.28192E-8;Km=5.964552E-3;Wmax=2618;Wmin=0;t0=0;g=9.82;
+dX = zeros(12,1);
+X =  num2cell(X);
+U = [1;1;1;1];
+
+[x, y, z, phi, theta, psi, xDot, yDot, zDot, phiDot, thetaDot, psiDot] = deal(X{:});
+
+
+n=12;
+kp = 1;
+kd = 1;
+
+desiredPts = zeros(3,n);
+desiredPts(1:3,1:3) = calcTraj(t0,runTime,t,P0,PF);
+Fx = m*(-kp*(x-desiredPts(1,1)) - kd*(xDot-desiredPts(1,2)) + desiredPts(1,3));
+Fy = m*(-kp*(y-desiredPts(2,1)) - kd*(yDot-desiredPts(2,2)) + desiredPts(2,3));
+
+thetaDesired = asin(Fx/U(1));
+phiDesired = asin(Fy/U(1));
+
+%Update desired points
+desiredPts(1,4:5) = [phiDesired,thetaDesired];
+
+
+
+B = zeros(2*n,n);
+B(n+1:end,:) = eye(n);
+
+phi = .10;
+
+e = zeros(2*n,1);
+
+if norm(e.'*P*B) > phi
+    Vr = -rho*(e.'*P*B)/(norm(e.'*P*B));
+else
+    Vr = -rho*(e.'*P*B)/(phi);
+end
+
+V = desiredPts(3,:).' - K*e + Vr.';
+
+
+Tau = 1;
+U(:,:) = 0;
+
+
+% Allocation Matrix
+allocMat = [1/(4*Kf*L), -sqrt(2)/(4*Kf*L), -sqrt(2)/(4*Kf*L), -1/(4*Km*Kf);
+            1/(4*Kf*L), -sqrt(2)/(4*Kf*L), sqrt(2)/(4*Kf*L), 1/(4*Km*Kf);
+            1/(4*Kf*L), sqrt(2)/(4*Kf*L), sqrt(2)/(4*Kf*L), -1/(4*Km*Kf);
+            1/(4*Kf*L), sqrt(2)/(4*Kf*L), -sqrt(2)/(4*Kf*L), 1/(4*Km*Kf)];
+
+Wdesired = sqrt(allocMat*U);
+
+Omega = Wdesired(1) - Wdesired(2) + Wdesired(3) - Wdesired(4);
+
+
+% Acclerations
+Xdd = [(1/m)*(cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi))*U(1);
+        (1/m)*(cos(phi)*sin(theta)*cos(psi) - sin(phi)*sin(psi))*U(1);
+        (1/m)*(cos(phi)*cos(theta))*U(1) - g;
+        thetaDot*psiDot*((Iy-Iz)/Ix) - (Ip/Ix)*Omega*thetaDot + (1/Ix)*U(2);
+        phiDot*psiDot*((Iz-Ix)/Iy) + (Ip/Iy)*Omega*phiDot + (1/Iy)*U(3);
+        phiDot*thetaDot*((Ix-Iy)/Iz) + (1/Iz)*U(4)];
+
+dX(1) = xDot;
+dX(2) = yDot;
+dX(3) = zDot;
+dX(4) = phiDot;
+dX(5) = thetaDot;
+dX(6) = psiDot;
+dX(7:12) = Xdd;
+
+end
 
 
 function ret = calcTraj(t0,tf,currT,P0,PF)
